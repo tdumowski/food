f97a7b7f20420da6b29dafe76e56a548f6d69b00
tdumowski <81873071+tdumowski@users.noreply.github.com>
2025-07-05 18:48:32 +0200

    28, Add new feature to process user input
    Introduced a function to handle and validate user input, improving the robustness of the input processing workflow.

--- resources/views/admin/body/footer.blade.php
+++ resources/views/admin/body/footer.blade.php
@@ -2,11 +2,11 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-sm-6">
-                <script>document.write(new Date().getFullYear())</script> © Minia.
+                <script>document.write(new Date().getFullYear())</script> © Tomasz Dumowski.
             </div>
             <div class="col-sm-6">
                 <div class="text-sm-end d-none d-sm-block">
-                    Design & Develop by <a href="#!" class="text-decoration-underline">Themesbrand</a>
+                    Design & Develop by <a href="#!" class="text-decoration-underline">Tomasz Dumowski</a>
                 </div>
             </div>
         </div>
