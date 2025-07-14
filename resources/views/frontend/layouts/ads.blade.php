6dfbf9ef3b33ff2b89516efb3f16f42b660b1811
tdumowski <81873071+tdumowski@users.noreply.github.com>
2025-07-14 11:48:01 +0200

    99, Show restaurant in frontend home page part 1

--- resources/views/frontend/layouts/ads.blade.php
+++ resources/views/frontend/layouts/ads.blade.php
@@ -1,26 +1,29 @@
+@php
+    $banners = \App\Models\Banner::orderBy('id', 'desc')
+        ->limit(4)
+        ->get();
+@endphp
+
 <section class="section pt-5 pb-5 bg-white homepage-add-section">
     <div class="container">
-    <div class="row">
-        <div class="col-md-3 col-6">
-            <div class="products-box">
-                <a href="listing.html"><img alt="" src="{{ asset('frontend/img/pro1.jpg') }}" class="img-fluid rounded"></a>
+        <div class="row">
+
+            @foreach ($banners as $banner)
+                <div class="col-md-3 col-6">
+                    <div class="products-box">
+                        <a href="{{ $banner->url }}" target="_blank">
+                            <img alt="{{ $banner->title }}" src="{{ asset($banner->image) }}" class="img-fluid rounded">
+                        </a>
+                    </div>
+                </div>
+                
+            @endforeach
+
+            <div class="col-md-3 col-6">
+                <div class="products-box">
+                    <a href="listing.html"><img alt="" src="{{ asset('frontend/img/pro1.jpg') }}" class="img-fluid rounded"></a>
+                </div>
             </div>
         </div>
-        <div class="col-md-3 col-6">
-            <div class="products-box">
-                <a href="listing.html"><img alt="" src="{{ asset('frontend/img/pro2.jpg') }}" class="img-fluid rounded"></a>
-            </div>
-        </div>
-        <div class="col-md-3 col-6">
-            <div class="products-box">
-                <a href="listing.html"><img alt="" src="{{ asset('frontend/img/pro3.jpg') }}" class="img-fluid rounded"></a>
-            </div>
-        </div>
-        <div class="col-md-3 col-6">
-            <div class="products-box">
-                <a href="listing.html"><img alt="" src="{{ asset('frontend/img/pro4.jpg') }}" class="img-fluid rounded"></a>
-            </div>
-        </div>
-    </div>
     </div>
 </section>
