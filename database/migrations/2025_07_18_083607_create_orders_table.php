b1055459c9037f3e28761e526e093836b7d35053
tdumowski <81873071+tdumowski@users.noreply.github.com>
2025-07-18 10:52:47 +0200

    135, Apply cash on delivery process part 2

--- database/migrations/2025_07_18_083607_create_orders_table.php
+++ database/migrations/2025_07_18_083607_create_orders_table.php
@@ -13,6 +13,27 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('payment_type')->nullable();
+            $table->string('payment_method');
+            $table->string('transaction_id')->nullable();
+            $table->string('currency')->nullable();
+            $table->float('amount', 8, 2);
+            $table->float('total_amount', 8, 2);
+            $table->string('order_number')->nullable();
+            $table->string('invoice')->nullable();
+            $table->string('order_date')->nullable();
+            $table->string('order_month')->nullable();
+            $table->string('order_year')->nullable();
+            $table->date('confirmed_date')->nullable();
+            $table->date('processing_date')->nullable();
+            $table->date('shipped_date')->nullable();
+            $table->date('delivered_date')->nullable();
+            $table->string('status');
             $table->timestamps();
         });
     }
