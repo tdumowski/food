425ba677c77022021659ac66927dabc3d47853b6
tdumowski <81873071+tdumowski@users.noreply.github.com>
2025-07-19 18:23:15 +0200

    152, Manage report system in admin panel part 2

--- resources/views/admin/backend/report/all_report.blade.php
+++ resources/views/admin/backend/report/all_report.blade.php
@@ -20,10 +20,125 @@
         </div>
         <!-- end page title -->
 
-        <div class="row row-cols-1 row-cols-md-1 row-cols-lg-2 row-cols-xl-2">
+        <div class="row">
             <div class="col">
                 <div class="card">
                     <div class="card-body">
+
+
+
+
+
+
+
+                        <div class="bg-light-subtle p-3">
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <form id="myForm" action="{{ route('store.category') }}" method="POST" enctype="multipart/form-data">
+                                        @csrf
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <div>
+                                                    <h5>Search by date</h5>
+                                                    <div class="form-group mb-3">
+                                                        <label for="example-text-input" class="form-label">Date</label>
+                                                        <input class="form-control" name="date" type="date" id="example-text-input">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mt-4">
+                                                <button type="submit" class="btn btn-primary waves-effect waves-light">Search</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+                                <div class="col-sm-4">
+                                    <form id="myForm" action="{{ route('store.category') }}" method="POST" enctype="multipart/form-data">
+                                        @csrf
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <div>
+                                                    <h5>Search by month</h5>
+                                                    <div class="form-group mb-3">
+                                                        <label for="example-text-input" class="form-label">Month</label>
+                                                        <select name="month_name" class="form-select">
+                                                            <option value="" disabled selected hidden>Select month</option>
+                                                            <option value="January">January</option>
+                                                            <option value="February">February</option>
+                                                            <option value="March">March</option>
+                                                            <option value="April">April</option>
+                                                            <option value="May">May</option>
+                                                            <option value="June">June</option>
+                                                            <option value="July">July</option>
+                                                            <option value="August">August</option>
+                                                            <option value="September">September</option>
+                                                            <option value="October">October</option>
+                                                            <option value="November">November</option>
+                                                            <option value="December">December</option>
+                                                        </select>
+
+                                                        <label for="example-text-input" class="form-label">Year</label>
+                                                        <select name="year_name" class="form-select">
+                                                            <option value="" disabled selected hidden>Select year</option>
+                                                            <option value="2022">2022</option>
+                                                            <option value="2023">2023</option>
+                                                            <option value="2024">2024</option>
+                                                            <option value="2025">2025</option>
+                                                            <option value="2026">2026</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mt-4">
+                                                <button type="submit" class="btn btn-primary waves-effect waves-light">Search</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+
+                                <div class="col-sm-4">
+                                    <form id="myForm" action="{{ route('store.category') }}" method="POST" enctype="multipart/form-data">
+                                        @csrf
+                                        <div class="row">
+                                            <div class="col-lg-12">
+                                                <div>
+                                                    <h5>Search by year</h5>
+                                                    <div class="form-group mb-3">
+                                                        <label for="example-text-input" class="form-label">Year</label>
+                                                        <select name="year" class="form-select">
+                                                            <option value="" disabled selected hidden>Select year</option>
+                                                            <option value="2022">2022</option>
+                                                            <option value="2023">2023</option>
+                                                            <option value="2024">2024</option>
+                                                            <option value="2025">2025</option>
+                                                            <option value="2026">2026</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="mt-4">
+                                                <button type="submit" class="btn btn-primary waves-effect waves-light">Search</button>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                     </div>
                 </div>
