5de73db151615bd6b9c0e3a45c790852448bad6d
tdumowski <81873071+tdumowski@users.noreply.github.com>
2025-07-19 08:39:11 +0200

    145, Manage order from restaurant panel part 2

--- resources/views/client/backend/order/all_orders.blade.php
+++ resources/views/client/backend/order/all_orders.blade.php
@@ -8,7 +8,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                    <h4 class="mb-sm-0 font-size-18">All client orders</h4>
+                    <h4 class="mb-sm-0 font-size-18">Client all orders</h4>
 
                     <div class="page-title-right">
                         <ol class="breadcrumb m-0">
@@ -38,14 +38,21 @@
                             </thead>
 
                             <tbody>
-                                @foreach($orders as $key => $order)
+                                @foreach($orderItemGroupData as $key => $orderItem)
+                                @php
+                                    $firstItem = $orderItem->first();
+                                    $order = $firstItem->order;
+                                @endphp
                                 <tr>
                                     <td>{{ $key + 1 }}</td>
                                     <td>{{ $order->order_date }}</td>
                                     <td>{{ $order->invoice_number }}</td>
                                     <td>{{ $order->total_amount }}</td>
                                     <td>{{ $order->payment_type }}</td>
-                                    <td><span class="badge bg-primary">{{ $order->status }}</span></td>
+                                    @php
+                                        $statusColor = ($order->status == "DELIVERED") ? "success" : "primary";
+                                    @endphp
+                                    <td><span class="badge bg-{{ $statusColor }}">{{ $order->status }}</span></td>
                                     <td>
                                         <a href="{{ route('admin.order_details', $order->id) }}" class="btn btn-info waves-effect waves-light"><i class="fas fa-eye"></i></a>
                                     </td>
