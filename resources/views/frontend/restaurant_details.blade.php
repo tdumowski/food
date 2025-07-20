9e58f25d09521f03c235fc42480aeb0425986302
tdumowski <81873071+tdumowski@users.noreply.github.com>
2025-07-20 13:51:15 +0200

    162, Restaurant review system part 4

--- resources/views/frontend/restaurant_details.blade.php
+++ resources/views/frontend/restaurant_details.blade.php
@@ -362,37 +362,58 @@
                                     <button type="button" class="btn btn-outline-primary btn-sm">Rate and Review</button>
                                 </div>
                             </div>
+                            
+
+
+                            
+                            
                             <div class="bg-white rounded shadow-sm p-4 mb-4 restaurant-detailed-ratings-and-reviews">
                                 <a href="#" class="btn btn-outline-primary btn-sm float-right">Top Rated</a>
                                 <h5 class="mb-1">All Ratings and Reviews</h5>
-                                <div class="reviews-members pt-4 pb-4">
-                                    <div class="media">
-                                    <a href="#"><img alt="Generic placeholder image" src="{{ asset('frontend/img/user/1.png') }}" class="mr-3 rounded-pill"></a>
-                                    <div class="media-body">
-                                        <div class="reviews-members-header">
-                                            <span class="star-rating float-right">
-                                            <a href="#"><i class="icofont-ui-rating active"></i></a>
-                                            <a href="#"><i class="icofont-ui-rating active"></i></a>
-                                            <a href="#"><i class="icofont-ui-rating active"></i></a>
-                                            <a href="#"><i class="icofont-ui-rating active"></i></a>
-                                            <a href="#"><i class="icofont-ui-rating"></i></a>
-                                            </span>
-                                            <h6 class="mb-1"><a class="text-black" href="#">Singh Osahan</a></h6>
-                                            <p class="text-gray">Tue, 20 Mar 2020</p>
-                                        </div>
-                                        <div class="reviews-members-body">
-                                            <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections </p>
-                                        </div>
-                                        <div class="reviews-members-footer">
-                                            <a class="total-like" href="#"><i class="icofont-thumbs-up"></i> 856M</a> <a class="total-like" href="#"><i class="icofont-thumbs-down"></i> 158K</a> 
+
+                                @php
+                                    $reviews = App\Models\Review::where('client_id', $client->id)->where('status', 1)->latest()->limit(5)->get()
+                                @endphp
+
+                                @foreach ($reviews as $review)
+                                    <div class="reviews-members pt-4 pb-4">
+                                        <div class="media">
+                                            <a href="#"><img alt="Generic placeholder image" 
+                                                src="{{ !empty($review->user->photo) ? url('upload/user_images/' . $review->user->photo) : url('upload/no_user.png') }}" 
+                                                class="mr-3 rounded-pill">
+                                            </a>
+                                            <div class="media-body">
+                                                <div class="reviews-members-header">
+                                                    <span class="star-rating float-right">
+                                                        <a href="#"><i class="icofont-ui-rating active"></i></a>
+                                                        <a href="#"><i class="icofont-ui-rating active"></i></a>
+                                                        <a href="#"><i class="icofont-ui-rating active"></i></a>
+                                                        <a href="#"><i class="icofont-ui-rating active"></i></a>
+                                                        <a href="#"><i class="icofont-ui-rating"></i></a>
+                                                    </span>
+                                                    <h6 class="mb-1"><a class="text-black" href="#">{{ $review->user->name }}</a></h6>
+                                                    <p class="text-gray">{{ Carbon::parse($review->created_at)->diffForHumans() }}</p>
+                                                </div>
+                                                <div class="reviews-members-body">
+                                                    <p>{{ $review->comment }}</p>
+                                                </div>
+                                                <div class="reviews-members-footer">
+                                                    <a class="total-like" href="#"><i class="icofont-thumbs-up"></i> 856M</a> <a class="total-like" href="#"><i class="icofont-thumbs-down"></i> 158K</a> 
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                    </div>
-                                </div>
+                                @endforeach
                                 <hr>
                                 <hr>
                                 <a class="text-center w-100 d-block mt-4 font-weight-bold" href="#">See All Reviews</a>
                             </div>
+
+
+
+
+
+
 
                             <div class="bg-white rounded shadow-sm p-4 mb-5 rating-review-select-page">
                                 @guest
